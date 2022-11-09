5fac37138c02f67b104331636610dbab1f224b64
时佰勇 <shibaiyong@example.com>
2022-11-09 19:48:49 +0800

    日历组件更新

--- src/components/calender/src/calender.vue
+++ src/components/calender/src/calender.vue
@@ -3,35 +3,40 @@
         <div class="calenderContainer" v-show="visible">
             <div class="title">
 
-                <a href="javascript:void(0);" class="prev" @click="changeMonth('prve')">上一月</a>
+                <a href="javascript:void(0);" class="prev" @click="changeMonth('prve')"><img src="@/assets/logImages/arrow_tip.png"/>上一月</a>
 
                 <span>{{title}}</span>
                 
-                <a href="javascript:void(0);" class="next" @click="changeMonth('next')">下一月</a>
+                <a href="javascript:void(0);" class="next" @click="changeMonth('next')">下一月<img src="@/assets/logImages/arrow_tip.png"/></a>
             </div>
-            <ul>
-                <li><span>日</span></li>
-                <li><span>一</span></li>
-                <li><span>二</span></li>
-                <li><span>三</span></li>
-                <li><span>四</span></li>
-                <li><span>五</span></li>
-                <li><span>六</span></li>
-                <!--<li><span>日</span></li>-->
-            </ul>
-            <ul class="dateTable">
-                <li v-for="item in showDate" :key="item.displayIntoText"  :class="[clickDay?'':item.now, item.day==clickDay?'clickActive':'']">
-                    <span v-if="item.month == item.currentMonth" @click="displayDate(item.displayIntoText, item.day)">{{item.day}}</span>
-                    <span v-else class='disableClick'>{{item.day}}</span>
-                    <label class="task">●</label>
-                </li>
-            </ul>
+            <div class="contentBox">
+              <ul>
+                  <li><span>日</span></li>
+                  <li><span>一</span></li>
+                  <li><span>二</span></li>
+                  <li><span>三</span></li>
+                  <li><span>四</span></li>
+                  <li><span>五</span></li>
+                  <li><span>六</span></li>
+                  <!--<li><span>日</span></li>-->
+              </ul>
+              <div class="tanslatewrraper" ref="translateW"  :style="{'height':translateH+'px'}">
+                <div class="translatecontent" ref="translate"  :style="{'top':translateT+'px'}">
+                  <ul class="dateTable" v-for="(items,index) in showDate" :key="index">
+                      <li v-for="item in items" :key="item.displayIntoText" :class="[clickDay?'':item.now, nowMonth==item.month&&item.day==clickDay?'clickActive':'']">
+                        <span v-if="!item.disabled" @click="displayDate(item.displayIntoText, item.day,$event)">{{item.day}}<label :class="{'red':item.task=='red','green':item.task=='green','white':item.task=='white'}" v-show="item.task">●</label></span>
+                        <span v-else class='disableClick'>{{item.day}}</span>
+                      </li>
+                  </ul>
+                </div>
+              </div>
+              <img @click="touchstart" :class="isopen?'opened':''" src="@/assets/logImages/log_downarrow@2x.png"/>
+            </div>
         </div>
     </div>  
 </template>
 <script>
-// import TreeContent from "@/components/common/TreeContent"
-
+/* eslint-disable */
 export default {
   name: "by-calender",
   props:{
@@ -59,12 +64,14 @@ export default {
       nowMonth: "",
       nowYear: "",
       displayIntoText: "",
-      clickDay: ""
+      clickDay: "",
+      pageY:0,
+      distance:0,
+      translateH:0,
+      translateT:0,
+      arrowTop:0,
+      isopen:true
     };
-  },
-  updated() {},
-  beforeCreate: function() {
-    // this.$options.components.TreeContent = () => import("@/components/common/TreeContent")
   },
   created() {
     this.initeNowDate();
@@ -72,13 +79,35 @@ export default {
   mounted() {
     //有默认值显示，无默认值该逻辑不执行。
     this.defaultValue ? this.backShowDate() : this.createdCalender(this.nowYear, this.nowMonth);
+    this.$nextTick(()=>{
+      this.translateH = this.$refs.translate.offsetHeight
+      this.checkCallend()
+    })
   },
   methods: {
+    touchstart(e){
+      
+      if(this.isopen){
+        this.translateH = 44
+        this.translateT = -(this.cellPPTop-2)
+      }else{
+        this.translateH = this.$refs.translate.offsetHeight
+        this.translateT = 0
+      }
+      this.isopen = !this.isopen
+    },
+    checkCallend(){
+      this.cellPPTop = document.querySelector('.active').offsetTop
+      this.translateT = -(this.cellPPTop-2)
+      this.translateH = 44
+      this.isopen = false
+    },
     initeNowDate() {
       let date = new Date();
       this.initeNowMonth = this.nowMonth = date.getMonth();
       this.initeNowYear = this.nowYear = date.getFullYear();
-      this.initeNowDate = this.nowDate = date.getDate();
+      this.initeNowDates = this.nowDate = date.getDate();
+      this.currentTime = this.createDateString(this.initeNowYear,this.initeNowMonth,this.initeNowDates)
       
     },
     changeMonth(type) {
@@ -121,49 +150,63 @@ export default {
     //根据日期的改变生成日历。
     createdCalender(year, month) {
       this.clickDay = '';//月份变化时去掉点击效果。
+      var disabled = false
+      var displayIntoText = ''
       var willCreatDate = [];
+      var temparr = [];
       var currentDate = new Date(year, month, 1);
-      var currentYear = currentDate.getFullYear();
-      var currentMonth = currentDate.getMonth();
+      var currentYear = this.nowYear = currentDate.getFullYear();
+      var currentMonth = this.nowMonth = currentDate.getMonth();
       //某个月一号是星期几
       var currentDay = currentDate.getDay();
       var now = "";
       var n = 1 - currentDay;
       this.title = currentYear + "年" + (currentMonth + 1) + "月";
-      this.$emit('clickMonBtnCB',{year:currentYear, month:currentMonth+1});
       //确定上个月有几天展示出来。
       n = n == 1 ? 1 : n;
       currentDate.setDate(n);
       for (var i = 0; i < 42; i++) {
+        
         var day = currentDate.getDate();
         var month = currentDate.getMonth();
-        if (
-          this.initeNowYear == currentYear &&
-          this.initeNowMonth == currentMonth &&
-          this.initeNowDate == day
-        ) {
+        var year = currentDate.getFullYear();
+        if (this.initeNowYear == year &&this.initeNowMonth == month && this.initeNowMonth == currentMonth&&this.initeNowDates == day) {
           now = "active";
         } else {
           now = "";
         }
-        willCreatDate.push({
-          day: day,
-          month: month,
+        displayIntoText = `${currentYear}-${month + 1 > 9 ? month + 1 : "0" + (month + 1)}-${day > 9 ? day : "0" + day}`
+
+        this.currentTimes = this.createDateString(year,month,day)
+
+        if(month != currentMonth || this.currentTime < this.currentTimes){
+          disabled = true
+        }else{
+          disabled = false
+        }
+        temparr.push({
+          day,
+          month,
+          year,
           currentMonth: currentMonth,
-          displayIntoText:
-            currentYear +
-            "-" +
-            (month + 1 > 9 ? month + 1 : "0" + (month + 1)) +
-            "-" +
-            (day > 9 ? day : "0" + day),
-          now: now
-        });
+          displayIntoText,
+          now: now,
+          disabled
+        })
+        if((i+1)%7==0){
+          willCreatDate.push(temparr);
+          temparr = []
+        }
+        
         currentDate.setDate(day + 1);
       }
       this.showDate = willCreatDate;
+      this.$emit('clickMonBtnCB',{year:currentYear, month:currentMonth+1});
     },
     //将选择的日期显示到输入框中
-    displayDate(text, day) {
+    displayDate(text, day, event) {
+      this.cellPPTop = event.target.parentNode.parentNode.offsetTop
+      this.cellPP = event.target.parentNode.parentNode
       var displayIntoText = text;
       var displayIntoTextValue = text;
       if(this.format != 'yyyy-MM-dd'){
@@ -172,6 +215,7 @@ export default {
       if(this.formatValue != 'yyyy-MM-dd'){
         displayIntoTextValue = this.dateFormat(text, 'formatValue');
       }
+
       this.clickDay = day; //给选中的日期加效果。
 
       //日历组件根据displayIntoText创建。
@@ -203,13 +247,44 @@ export default {
       }
       return format;
     },
-    showDefaultDate(){
-
+    createDateString(year,month,day){
+      return `${year}${month + 1 > 9 ? month + 1 : "0" + (month + 1)}${day > 9 ? day : "0" + day}`
+    },
+    addTask(year, month, arr){
+      let showDate = JSON.parse(JSON.stringify(this.showDate))
+      let len = showDate.length
+      let klen = arr.length
+      let jlen=0,obj = null,items=[]
+      for(let i = 0; i < len; i++){
+        items = showDate[i]
+        jlen = items.length
+        for(let j = 0; j < jlen; j++){
+          obj = items[j]
+          if(year == obj.year && month == obj.month){
+            for(var k = 0; k < klen; k++){
+              if(arr[k].day == obj.day){
+                obj.task = arr[k].color
+              }
+            }
+          }
+        }
+      }
+      this.showDate = showDate
+    }
+  },
+  watch:{
+    defaultValue(newVal){
+      // console.log()
+      // this.initeNowDate()
+      // this.defaultValue ? this.backShowDate() : this.createdCalender(this.nowYear, this.nowMonth)
     }
   }
 };
 </script>
 <style scoped>
+.opened{
+  transform: rotateZ(180deg);
+}
 .inputCalender input {
   padding-left: 0.2rem;
   margin: 0;
@@ -220,73 +295,142 @@ export default {
   outline: none;
   outline: 0px;
 }
+.contentBox{
+  padding:.4rem .3rem 0;
+  font-size:0;
+  text-align: center;
+  perspective:800px;
+}
 ul {
   overflow: hidden;
   list-style: none;
+  display: flex;
+  justify-content: space-between;
 }
 
 ul li {
-  float: left;
-  width: 14%;
-  list-style: none;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  width: .64rem;
+  height:.64rem;
+  /* overflow:hidden; */
+  border-radius: .32rem;
 }
 ul li span {
-  display: inline-block;
-  width: 100%;
-  padding: 0.1rem 0;
+  display: block;
   text-align: center;
   cursor: pointer;
-  border-radius: 50%;
-  font-size:0.32rem;
+  font-size:0.28rem;
+  color: #B2B2B2;
 }
-.dateTable li label{
-  font-size:0.24rem;
-  color:red;
+.dateTable{
+  font-size:.28rem;
+}
+.dateTable li{
+  margin-bottom:0.2rem;
+  border-radius: 50%;
+  
+}
+.dateTable li span{
+  color:#222;
+  line-height: .64rem;
+  position: relative;
+}
+.dateTable li span label{
+  display: block;
+  font-size:0.1rem;
+  color:#FC6450;
+  text-align: center;
+  position: absolute;
+  bottom:0;
+  left:50%;
+  line-height: .24rem;
+  margin-left:-0.08rem;
+  margin-top:0.04rem;
 }
 .calenderWrapper {
   /* width: 4.6rem; */
   margin: 0 auto;
 }
+.calenderContainer .title span{
+  color:#222;
+  font-weight: blod;
+}
 .calenderContainer .title {
-  background: #eee;
+  background: #F4F5F6;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  /* padding:0 4%; */
   box-sizing: border-box;
-  font-size:0.4rem;
+  font-size:0.28rem;
+  height:.88rem;
+  color:#222;
+  padding:0 .24rem;
 }
 .calenderContainer .title a {
   display: inline-block;
   text-decoration: none;
-  padding:0.2rem;
-  background: #ddd;
   border-radius: 4px;
+  font-size:.24rem;
+  color:#888;
+}
+
+.calenderContainer .title a img{
+  width:.12rem;
+  margin-left: .12rem;
+}
+
+.calenderContainer .title .prev img{
+  transform: rotateY(180deg);
+  vertical-align: middle;
+  margin:0;
+  margin-right: .12rem;
 }
 
 .calenderContainer {
-  border: 1px solid #ddd;
+  background:white;
 }
-.active span{
-  background: rgba(161, 0, 233, 0.5);
+.active{
+  background: #FC6450;
+  
+}
+
+.dateTable .active span{
   color: white;
 }
-.clickActive span{
-  background: rgba(161, 0, 233, 0.5);
+.clickActive{
+  background: #FC6450;
 }
-
-.active .task{
+.dateTable .clickActive span{
   color:white;
 }
-
-.clickActive .task{
-  color:white;
+.dateTable .red{
+  color:red;
 }
 
-.disableClick {
+.dateTable .white{
+  color:#ddd;
+}
+
+.dateTable .green{
+  color:green;
+}
+
+.dateTable .disableClick {
   color: #ddd;
+}
+
+.tanslatewrraper{
+  overflow: hidden;
+  text-align: center;
+}
+
+.translatecontent{
+  position: relative;
+}
+
+.contentBox img{
+  width:.58rem;
+  position: relative;
+  display: inline-block;
+  padding:0.1rem 0;
 }
 </style>
